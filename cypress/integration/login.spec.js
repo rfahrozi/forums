bbd8b2c33cca7ee6c136fdf3241c76788714026f
rfahrozi <fahrozi@gmail.com>
2025-10-02 12:50:43 +0700

    Fix test back for CI pass

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -31,7 +31,7 @@ describe('Login Flow', () => {
     cy.url().should('include', '/');
 
     // Memastikan navigasi tidak menampilkan login (artinya logout muncul)
-    cy.contains('Masuk').should('be.visible');
+    cy.contains('Masuk').should('not.exist');
   });
 
   it('should show error on invalid login', () => {
